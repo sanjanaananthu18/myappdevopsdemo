6d8b2da93f4197a0edf0efb43f4d56358dd1cadc
sanjanaananthu18 <sanjanaananthu18@gmail.com>
2024-10-17 15:42:55 +0530

    first commit

---
+++ myappdemodevops/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace myappdemodevops.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
